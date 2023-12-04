bf50a03fe3199a585340453ead4e91458817f3a4
narugo1992 <narugo992@gmail.com>
2023-12-04 18:02:01 +0800

    dev(narugo): fix it

--- db.py
+++ db.py
@@ -1,10 +1,14 @@
 import logging
 
+from waifuc.action import ModeConvertAction, AlignMinSizeAction
 from waifuc.source import DanbooruSource
 
 logging.basicConfig(level=logging.DEBUG)
 
 for item in DanbooruSource(
         ['animated_gif'],
-)[:30]:
+)[:30].attach(
+    ModeConvertAction(),
+    AlignMinSizeAction(640),
+):
     pass
